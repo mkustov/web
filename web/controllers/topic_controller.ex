2f99678ebe92f436efb0da8cbeb936d33d27a0ec
Maxim Kustov <maksym.kustov@gmail.com>
2017-07-27 17:48:10 +0200

    Add: index action, RM: page controller and stuff

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,13 +3,27 @@ defmodule Web.TopicController do
 
   alias Web.Topic
 
-  def new(conn, params) do
+  def index(conn, _params) do
+    render conn, "index.html"
+    topics = Repo.all(Topic)
+  end
+
+  def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
     render conn, "new.html", changeset: changeset
   end
 
   def create(conn, %{"topic" => topic}) do
-
+    changeset = Topic.changeset(%Topic{}, topic)
+    case Repo.insert(changeset) do
+      {:ok, post} ->
+        render conn, "index.html"
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
+    # if changeset.valid?
+    #   render conn, "index.html"
+    # end
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -16,7 +16,7 @@ defmodule Web.Router do
   scope "/", Web do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/", TopicController, :index
     get "topics/new", TopicController, :new
     post "topics", TopicController, :create
   end
